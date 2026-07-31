442aad80f51176b0f75c8ef195a3c3f2b73ba0dc
Tanner Kilian <tkilian12@gmail.com>
2026-07-31 00:08:58 -0700

    Added workspace layout switcher and a keybind

--- .config/hypr/keybinds.lua
+++ .config/hypr/keybinds.lua
@@ -17,6 +17,34 @@ local function layout_aware_dispatch(actions)
   end
 end
 
+hl.bind(mainMod .. " + SPACE", function ()
+  local layouts = { "master", "scrolling", "monocle" }
+  local workspace = hl.get_active_workspace()
+  if hl.get_active_special_workspace() then
+    workspace = hl.get_active_special_workspace()
+  end
+
+  local next_layout = "master"
+
+  if not workspace then
+    return
+  end
+
+  for i = 1, #layouts do
+    if layouts[i] == workspace.tiled_layout then
+      local next_layout_idx = (i % #layouts) + 1
+      next_layout = layouts[next_layout_idx]
+      break
+    end
+  end
+
+  if workspace.special then
+    hl.workspace_rule({ workspace = tostring(workspace.name), layout = next_layout })
+  else
+    hl.workspace_rule({ workspace = tostring(workspace.id), layout = next_layout })
+  end
+end)
+
 
 -----------------------
 --- ESSENTIAL BINDS ---
